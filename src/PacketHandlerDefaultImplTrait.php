f9976e5b3c7495d39c6113a5c83654f8c16cf5f0
GameParrot <85067619+GameParrot@users.noreply.github.com>
2025-11-02 09:31:28 -0500

    remove old packet

--- src/PacketHandlerDefaultImplTrait.php
+++ src/PacketHandlerDefaultImplTrait.php
@@ -309,10 +309,6 @@ trait PacketHandlerDefaultImplTrait{
 		return false;
 	}
 
-	public function handleItemFrameDropItem(ItemFrameDropItemPacket $packet) : bool{
-		return false;
-	}
-
 	public function handleGameRulesChanged(GameRulesChangedPacket $packet) : bool{
 		return false;
 	}
