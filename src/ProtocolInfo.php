90a3e60f2f8a6cb7e0aa3e7ecf6c4dece9f96137
GameParrot <85067619+GameParrot@users.noreply.github.com>
2026-05-03 15:36:11 -0400

    revert

--- src/ProtocolInfo.php
+++ src/ProtocolInfo.php
@@ -33,8 +33,7 @@ final class ProtocolInfo{
 
 	/** Actual Minecraft: PE protocol version */
 
-	public const PROTOCOL_1_26_20_1 = 975;
-	public const PROTOCOL_1_26_20 = 974;
+	public const PROTOCOL_1_26_20 = 975;
 	public const PROTOCOL_1_26_10 = 944;
 	public const PROTOCOL_1_26_0 = 924;
 	public const PROTOCOL_1_21_130 = 898;
@@ -118,7 +117,6 @@ final class ProtocolInfo{
 		self::PROTOCOL_1_21_130,
 		self::PROTOCOL_1_26_0,
 		self::PROTOCOL_1_26_20,
-		self::PROTOCOL_1_26_20_1,
 		self::CURRENT_PROTOCOL
 	];
 
