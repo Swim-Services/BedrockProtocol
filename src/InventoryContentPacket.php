2d7d4bdbae59f5927f7b072cddc4e3e3c857200a
GameParrot <85067619+GameParrot@users.noreply.github.com>
2026-05-19 11:29:10 -0400

    dont think these are optional

--- src/InventoryContentPacket.php
+++ src/InventoryContentPacket.php
@@ -19,7 +19,6 @@ use pmmp\encoding\ByteBufferWriter;
 use pmmp\encoding\VarInt;
 use pocketmine\network\mcpe\protocol\serializer\CommonTypes;
 use pocketmine\network\mcpe\protocol\types\inventory\FullContainerName;
-use pocketmine\network\mcpe\protocol\types\inventory\ItemStack;
 use pocketmine\network\mcpe\protocol\types\inventory\ItemStackWrapper;
 use function count;
 
@@ -53,15 +52,10 @@ class InventoryContentPacket extends DataPacket implements ClientboundPacket{
 		for($i = 0; $i < $count; ++$i){
 			$this->items[] = $protocolId >= ProtocolInfo::PROTOCOL_1_26_30 ? CommonTypes::getNetworkItemStackDescriptor($in) : CommonTypes::getItemStackWrapper($in);
 		}
-		if ($protocolId >= ProtocolInfo::PROTOCOL_1_26_30) {
-			$containerName = CommonTypes::readOptional($in, fn(ByteBufferReader $in) => FullContainerName::read($in, $protocolId));
-			$this->containerName = $containerName ?? new FullContainerName(0);
-			$storage = CommonTypes::readOptional($in, CommonTypes::getNetworkItemStackDescriptor(...));
-			$this->storage = $storage ?? new ItemStack(0, 0, 0, 0, "");
-		} elseif($protocolId >= ProtocolInfo::PROTOCOL_1_21_30){
+		if($protocolId >= ProtocolInfo::PROTOCOL_1_21_30){
 			$this->containerName = FullContainerName::read($in, $protocolId);
 			if($protocolId >= ProtocolInfo::PROTOCOL_1_21_40){
-				$this->storage = CommonTypes::getItemStackWrapper($in);
+				$this->storage = $protocolId >= ProtocolInfo::PROTOCOL_1_26_30 ? CommonTypes::getNetworkItemStackDescriptor($in) : CommonTypes::getItemStackWrapper($in);
 			}else{
 				$this->dynamicContainerSize = VarInt::readUnsignedInt($in);
 			}
@@ -76,17 +70,10 @@ class InventoryContentPacket extends DataPacket implements ClientboundPacket{
 		foreach($this->items as $item){
 			$protocolId >= ProtocolInfo::PROTOCOL_1_26_30 ? CommonTypes::putNetworkItemStackDescriptor($out, $item) : CommonTypes::putItemStackWrapper($out, $item);
 		}
-		if($protocolId >= ProtocolInfo::PROTOCOL_1_26_30){
-			CommonTypes::writeOptional($out, $this->containerName, fn(ByteBufferWriter $out, FullContainerName $v) => $v->write($out, $protocolId));
-			if ($this->storage->getItemStack()->isNull()) {
-				CommonTypes::putBool($out, false);
-			} else {
-				CommonTypes::writeOptional($out, $this->storage, CommonTypes::putNetworkItemStackDescriptor(...));
-			}
-		}elseif($protocolId >= ProtocolInfo::PROTOCOL_1_21_30){
+		if($protocolId >= ProtocolInfo::PROTOCOL_1_21_30){
 			$this->containerName->write($out, $protocolId);
 			if($protocolId >= ProtocolInfo::PROTOCOL_1_21_40){
-				CommonTypes::putItemStackWrapper($out, $this->storage);
+				$protocolId >= ProtocolInfo::PROTOCOL_1_26_30 ? CommonTypes::putNetworkItemStackDescriptor($out, $this->storage) : CommonTypes::putItemStackWrapper($out, $this->storage);
 			}else{
 				VarInt::writeUnsignedInt($out, $this->dynamicContainerSize);
 			}
